9f63bc87dab02afdacb8a5859c14bc1598618c60
tonydattolo <anthonydattolo@gmail.com>
2022-02-19 14:32:12 -0500

    brownie accounts

---
+++ brownieSimpleStorage/scripts/deploy.py
@@ -0,0 +1,23 @@
+from brownie import accounts, config
+import os
+
+
+def deploy_simple_storage():
+    # this will grab the local brownie ganache instance accounts to use for testing
+    account_0 = accounts[0]
+    print(f'{account_0} sample account from local brownie instance ganache-cli')
+    
+    # this will prompt you for the passphrase used to encrypt the account
+    sampleGanache9 = accounts.load("sampleGanache9")
+    print(f'{sampleGanache9} sample account from added private key')
+    
+    # can programatically add accounts to brownie.accounts from env vars
+    sampleGanache9fromEnv = accounts.add(os.getenv("SAMPLE_GANACHE9_PRIVATE_KEY"))
+    print(f'{sampleGanache9fromEnv} sample account from private key in env')    
+    
+    # can add accounts using yaml settings
+    sampleFromYaml = accounts.add(config["wallets"]["SAMPLE_GANACHE9_PRIVATE_KEY"])
+    # sampleFromYaml = accounts.add(config["wallets"]["sampleGanache9"]["privateKey"])
+
+def main():
+    deploy_simple_storage()
---
+++ brownieSimpleStorage/scripts/read_value.py
@@ -0,0 +1,7 @@
+from brownie import SimpleStorage, accounts, config
+
+def read_contract():
+    pass
+
+def main():
+    read_contract()
